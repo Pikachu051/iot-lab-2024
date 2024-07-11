4df18e4cba0f7635891d0d054daaa64de0b90e66
Sahachindech Katedee <86822195+Pikachu051@users.noreply.github.com>
2024-07-11 19:27:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,12 +36,29 @@ async def get_books(db: Session = Depends(get_db)):
     return db.query(models.Book).all()
 
 @router_v1.get('/books/{book_id}')
-async def get_book(book_id: int, db: Session = Depends(get_db)):
+async def get_book(response: Response, book_id: int, db: Session = Depends(get_db)):
+    if db.query(models.Book).filter(models.Book.id == book_id).first() is None:
+        response.status_code = 404
+        return {
+            'message': 'Book not found'
+        }
     return db.query(models.Book).filter(models.Book.id == book_id).first()
 
 @router_v1.post('/books')
 async def create_book(book: dict, response: Response, db: Session = Depends(get_db)):
     # TODO: Add validation
+
+    if 'title' not in book or 'author' not in book or 'year' not in book or 'is_published' not in book:
+        response.status_code = 400
+        return {
+            'message': 'Required data is missing'
+        }
+    elif db.query(models.Book).filter(models.Book.title == book['title']).first() is not None:
+        response.status_code = 409
+        return {
+            'message': 'Book already exists'
+        }
+
     newbook = models.Book(title=book['title'], author=book['author'], year=book['year'], is_published=book['is_published'])
     db.add(newbook)
     db.commit()
@@ -50,7 +67,12 @@ async def create_book(book: dict, response: Response, db: Session = Depends(get_
     return newbook
 
 @router_v1.patch('/books/{book_id}')
-async def update_book(book_id: int, book: dict, db: Session = Depends(get_db)):
+async def update_book(response: Response, book_id: int, book: dict, db: Session = Depends(get_db)):
+    if db.query(models.Book).filter(models.Book.id == book_id).first() is None:
+        response.status_code = 404
+        return {
+            'message': 'Book not exists'
+        }
     db.query(models.Book).filter(models.Book.id == book_id).update(book)
     db.commit()
     return {
@@ -58,7 +80,13 @@ async def update_book(book_id: int, book: dict, db: Session = Depends(get_db)):
     }
 
 @router_v1.delete('/books/{book_id}')
-async def delete_book(book_id: int, db: Session = Depends(get_db)):
+async def delete_book(response: Response, book_id: int, db: Session = Depends(get_db)):
+    if db.query(models.Book).filter(models.Book.id == book_id).first() is None:
+        response.status_code = 404
+        return {
+            'message': 'Book not exists'
+        }
+
     db.query(models.Book).filter(models.Book.id == book_id).delete()
     db.commit()
     return {
@@ -72,11 +100,28 @@ async def get_students(db: Session = Depends(get_db)):
     return db.query(models.Student).all()
 
 @router_v1.get('/students/{student_id}')
-async def get_student(student_id: int, db: Session = Depends(get_db)):
+async def get_student(response: Response, student_id: int, db: Session = Depends(get_db)):
+    if db.query(models.Student).filter(models.Student.id == student_id).first() is None:
+        response.status_code = 404
+        return {
+            'message': 'Student not found'
+        }
     return db.query(models.Student).filter(models.Student.id == student_id).first()
 
 @router_v1.post('/students')
 async def create_student(student: dict, response: Response, db: Session = Depends(get_db)):
+
+    if 'firstname' not in student or 'lastname' not in student or 'dob' not in student or 'id' not in student:
+        response.status_code = 400
+        return {
+            'message': 'Required data is missing'
+        }
+    elif db.query(models.Student).filter(models.Student.id == student['id']).first() is not None:
+        response.status_code = 409
+        return {
+            'message': 'Student already exists'
+        }
+
     newstudent = models.Student(firstname = student['firstname'], lastname = student['lastname'], dob = student['dob'], id = student['id'], gender = student['gender'])
     db.add(newstudent)
     db.commit()
@@ -85,7 +130,12 @@ async def create_student(student: dict, response: Response, db: Session = Depend
     return newstudent
 
 @router_v1.delete('/students/{student_id}')
-async def delete_student(student_id: int, db: Session = Depends(get_db)):
+async def delete_student(response: Response, student_id: int, db: Session = Depends(get_db)):
+    if db.query(models.Student).filter(models.Student.id == student_id).first() is None:
+        response.status_code = 404
+        return {
+            'message': 'Student not exists'
+        }
     db.query(models.Student).filter(models.Student.id == student_id).delete()
     db.commit()
     return {
@@ -93,7 +143,12 @@ async def delete_student(student_id: int, db: Session = Depends(get_db)):
     }
 
 @router_v1.patch('/students/{student_id}')
-async def update_student(student_id: int, student: dict, db: Session = Depends(get_db)):
+async def update_student(response: Response, student_id: int, student: dict, db: Session = Depends(get_db)):
+    if db.query(models.Student).filter(models.Student.id == student_id).first() is None:
+        response.status_code = 404
+        return {
+            'message': 'Student not exists'
+        }
     db.query(models.Student).filter(models.Student.id == student_id).update(student)
     db.commit()
     return {
